16cf1b219be00d8f5610612c3a15288c4303743f
Rich Trevillian <landrum.trevillian@gmail.com>
2018-10-11 18:01:51 -0400

    BandsInTown

--- liri.js
+++ liri.js
@@ -10,35 +10,63 @@
 
 // ===============================
 
-// 9. Make it so liri.js can take in one of the following commands:
-//    * `concert-this`
-//    * `spotify-this-song`
-//    * `movie-this`
-//    * `do-what-it-says`
+// require the REQUST module to make HTTP requests
+var request = require('request');
 
+// require the MOMENT module to format datetime objects into desired output format
+var moment = require('moment');
+
+// ===============================
 
 var command = process.argv[2];
 var mediaName = process.argv[3];
 
-if( command === 'concert-this' ) {
+if (command === 'concert-this') {
+    // console.log(command);
+    // console.log(mediaName);
+
+    var bandsInTownAPI = "https://rest.bandsintown.com/artists/" + mediaName + "/events?app_id=7ed0c7d7d54a0ea01c5b00adf0d359ea";
+    // console.log(bandsInTownAPI);
+
+    request(bandsInTownAPI, function (error, response, body) {
+
+        // console.log('error:', error); 
+        // console.log('statusCode:', response && response.statusCode); 
+        // console.log('body:', body); 
+
+        // use JSON.parse(body) to turn the returned JSON data body into a normal Object
+        var bodyData = JSON.parse(body);
+        // console.log('new bodyData object:', bodyData); 
+
+        // make variables for properties of the new bodyData whose values you want to use
+        var venueName = bodyData[0].venue.name;
+        var venueLocation = bodyData[0].venue.city;
+        // use MOMENT.JS to format the datetime data received into human-readable data
+        var venueDate = moment(bodyData[0].datetime, moment.ISO_8601).format('MMMM Do YYYY, h:mm a');
+
+        // output the requested infomation back to the CLI
+        console.log(`${mediaName} are playing at the ${venueName} in ${venueLocation} on ${venueDate}`);
+
+        // node liri.js concert-this "Slowdive"
+        // node liri.js concert-this "Man Without Country"
+        // node liri.js concert-this "Mumford & Sons"
+
+    });
+
+
+} else if (command === 'spotify-this-song') {
     console.log(command);
     console.log(mediaName);
 
 
-} else if( command === 'spotify-this-song') {
+} else if (command === 'movie-this') {
     console.log(command);
     console.log(mediaName);
 
 
-} else if( command === 'movie-this') {
+} else if (command === 'do-what-it-says') {
     console.log(command);
     console.log(mediaName);
 
 
-} else if( command === 'do-what-it-says') {
-    console.log(command);
-    console.log(mediaName);
-
-    
 }
-
