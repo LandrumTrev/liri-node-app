d17cef023236d54f8fae3768d29a79f594debd85
Rich Trevillian <landrum.trevillian@gmail.com>
2018-10-13 23:03:56 -0400

    separate API functions

--- liri.js
+++ liri.js
@@ -1,3 +1,15 @@
+// ====================================================
+// LIRI BOT node app
+// ©2018 Richard Trevillian
+// University of Richmond (Virginia)
+// Full Stack Developer Bootcamp (July 2018)
+// ====================================================
+
+
+// ====================================================
+// REQURED MODULES BELOW
+// ====================================================
+
 // require FS to write to an external log files
 var fs = require("fs");
 
@@ -16,7 +28,6 @@ var keys = require("./keys.js");
 // and then access the Spotify data in the required ./keys.js with keys.spotify
 var spotify = new Spotify(keys.spotify);
 // console.log(spotify);
-
 
 // ===============================
 
@@ -40,7 +51,38 @@ var divider = "\n\n=================================\n\n"
 
 // ===============================
 
-if (command === 'do-what-it-says') {
+
+
+// ====================================================
+// FUNCTION CHOOSER BASED ON INPUT ARGS
+// ====================================================
+
+
+if (command === 'rando') {
+
+    randoData();
+
+} else if (command === 'concert-this') {
+
+    getBandsintownData(mediaName);
+
+} else if (command === 'spotify-this-song') {
+
+    getSpotifyData(mediaName);
+
+} else if (command === 'movie-this') {
+
+    getOMDbData(mediaName);
+
+}
+
+
+
+// ====================================================
+// RANDO BELOW
+// ====================================================
+
+function randoData() {
 
     // use FS to read the random.txt file
     fs.readFile('./random.txt', 'utf8', function read(err, data) {
@@ -117,38 +159,19 @@ if (command === 'do-what-it-says') {
         // console.log(command);
         // console.log(mediaName);
 
+        // node liri.js rando
     };
-
-    // node liri.js do-what-it-says
-}
+};
 
 
-if (command === 'concert-this') {
+// ====================================================
+// BANDSINTOWN BELOW
+// ====================================================
 
-    // node liri.js concert-this Dead Can Dance
-    // node liri.js concert-this The Birthday Massacre
-    // node liri.js concert-this Thievery Corporation
-    // node liri.js concert-this Sting
-    // node liri.js concert-this Sigur Ros
-    // node liri.js concert-this Royksopp
-    // node liri.js concert-this Nine Inch Nails
-    // node liri.js concert-this Neko Case
-    // node liri.js concert-this New Order
-    // node liri.js concert-this Mylène Farmer
-    // node liri.js concert-this Morrissey
-    // node liri.js concert-this Michael Brook
-    // node liri.js concert-this M83
-    // node liri.js concert-this Lykke Li
-    // node liri.js concert-this Hooverphonic
-    // node liri.js concert-this Grimes
-    // node liri.js concert-this God Is an Astronaut
-    // node liri.js concert-this The Church
-    // node liri.js concert-this Above & Beyond
-    // node liri.js concert-this Slowdive
-    // node liri.js concert-this Man Without Country
-    // node liri.js concert-this Mumford & Sons
 
-    var bandsInTownAPI = "https://rest.bandsintown.com/artists/" + mediaName + "/events?app_id=7ed0c7d7d54a0ea01c5b00adf0d359ea";
+function getBandsintownData(mName) {
+
+    var bandsInTownAPI = "https://rest.bandsintown.com/artists/" + mName + "/events?app_id=7ed0c7d7d54a0ea01c5b00adf0d359ea";
     // console.log(bandsInTownAPI);
 
     request(bandsInTownAPI, function (error, response, body) {
@@ -174,28 +197,21 @@ if (command === 'concert-this') {
 
     });
 
+};
 
-} else if (command === 'spotify-this-song') {
 
-    // node liri.js spotify-this-song Cruel Summer
-    // node liri.js spotify-this-song As Long as You Follow 
-    // node liri.js spotify-this-song Bizarre Love Triangle 
-    // node liri.js spotify-this-song Voices Carry
-    // node liri.js spotify-this-song Year of the Cat
-    // node liri.js spotify-this-song A Preacher in New England 
-    // node liri.js spotify-this-song Before Barbed Wire 
-    // node liri.js spotify-this-song Clumsy Sky 
-    // node liri.js spotify-this-song Radau 
-    // node liri.js spotify-this-song World Princess part II 
-    // node liri.js spotify-this-song Algebra of Darkness 
-    // node liri.js spotify-this-song Carolyns Fingers 
-    // node liri.js spotify-this-song Avatar 
-    // node liri.js spotify-this-song Oregon Hill 
-    // node liri.js spotify-this-song Shape of My Heart 
+
+// ====================================================
+// SPOTIFY BELOW
+// ====================================================
+
+
+
+function getSpotifyData(mName) {
 
     var spotifyAPI;
 
-    if (!mediaName) {
+    if (!mName) {
 
         // then get Rick Rolled: Never Gonna Give You Up by Rick Astley
         spotifyAPI = "https://api.spotify.com/v1/search?q=track%3ANever%20Gonna%20Give%20You%20Up%20artist%3ARick%20Astley&type=track";
@@ -204,7 +220,7 @@ if (command === 'concert-this') {
         // spotifyAPI = "https://api.spotify.com/v1/search?q=track%3AThe%20Sign%20artist%3AAce%20Of%20Base&type=track";
 
     } else {
-        spotifyAPI = "https://api.spotify.com/v1/search?q=" + mediaName + "&type=track";
+        spotifyAPI = "https://api.spotify.com/v1/search?q=" + mName + "&type=track";
     }
 
     spotify
@@ -241,35 +257,22 @@ if (command === 'concert-this') {
             console.error('Error occurred: ' + err);
         });
 
+};
 
-} else if (command === 'movie-this') {
 
-    // node liri.js movie-this The Big Blue 
-    // node liri.js movie-this Leon The Professional 
-    // node liri.js movie-this La Femme Nikita 
-    // node liri.js movie-this 2001 A Space Odyssey 
-    // node liri.js movie-this Amelie 
-    // node liri.js movie-this Avatar 
-    // node liri.js movie-this Bolero 
-    // node liri.js movie-this Despicable Me 
-    // node liri.js movie-this Euro Trip 
-    // node liri.js movie-this The Last Mimzy 
-    // node liri.js movie-this The Life Aquatic with Steve Zissou 
-    // node liri.js movie-this The Princess Bride 
-    // node liri.js movie-this The Pink Panther 
-    // node liri.js movie-this The Wicker Man 
-    // node liri.js movie-this Tropic Thunder 
-    // node liri.js movie-this Watership Down 
-    // node liri.js movie-this The X-Files Fight the Future 
-    // node liri.js movie-this The Jerk 
-    // node liri.js movie-this The Hunger 
-    // node liri.js movie-this Talladega Nights 
-    // node liri.js movie-this Straight to Hell 
-    // node liri.js movie-this Spaceballs 
+
+// ====================================================
+// OMDb BELOW
+// ====================================================
+
+
+
+// function that calls the OMDb API
+function getOMDbData(mName) {
 
     var OMDbAPI;
 
-    if (!mediaName) {
+    if (!mName) {
 
         // Mr. Nobody
         // OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=a7b952c&t=Mr. Nobody";
@@ -278,7 +281,7 @@ if (command === 'concert-this') {
         OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=a7b952c&t=The Big Blue";
 
     } else {
-        OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=a7b952c&t=" + mediaName;
+        OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=a7b952c&t=" + mName;
     }
 
     request(OMDbAPI, function (error, response, body) {
@@ -322,7 +325,77 @@ if (command === 'concert-this') {
             if (err) throw err;
             console.log(movieData);
         });
-
     });
+};
 
-}
+
+
+// ====================================================
+// SAMPLE ENTRIES FROM/FOR THE random.txt FILE
+// ====================================================
+
+// node liri.js rando
+
+// node liri.js concert-this Dead Can Dance
+// node liri.js concert-this The Birthday Massacre
+// node liri.js concert-this Thievery Corporation
+// node liri.js concert-this Sting
+// node liri.js concert-this Sigur Ros
+// node liri.js concert-this Royksopp
+// node liri.js concert-this Nine Inch Nails
+// node liri.js concert-this Neko Case
+// node liri.js concert-this New Order
+// node liri.js concert-this Mylène Farmer
+// node liri.js concert-this Morrissey
+// node liri.js concert-this Michael Brook
+// node liri.js concert-this M83
+// node liri.js concert-this Lykke Li
+// node liri.js concert-this Hooverphonic
+// node liri.js concert-this Grimes
+// node liri.js concert-this God Is an Astronaut
+// node liri.js concert-this The Church
+// node liri.js concert-this Above & Beyond
+// node liri.js concert-this Slowdive
+// node liri.js concert-this Man Without Country
+// node liri.js concert-this Mumford & Sons
+
+// node liri.js spotify-this-song Cruel Summer
+// node liri.js spotify-this-song As Long as You Follow 
+// node liri.js spotify-this-song Bizarre Love Triangle 
+// node liri.js spotify-this-song Voices Carry
+// node liri.js spotify-this-song Year of the Cat
+// node liri.js spotify-this-song A Preacher in New England 
+// node liri.js spotify-this-song Before Barbed Wire 
+// node liri.js spotify-this-song Clumsy Sky 
+// node liri.js spotify-this-song Radau 
+// node liri.js spotify-this-song World Princess part II 
+// node liri.js spotify-this-song Algebra of Darkness 
+// node liri.js spotify-this-song Carolyns Fingers 
+// node liri.js spotify-this-song Avatar 
+// node liri.js spotify-this-song Oregon Hill 
+// node liri.js spotify-this-song Shape of My Heart 
+
+// node liri.js movie-this The Big Blue 
+// node liri.js movie-this Leon The Professional 
+// node liri.js movie-this La Femme Nikita 
+// node liri.js movie-this 2001 A Space Odyssey 
+// node liri.js movie-this Amelie 
+// node liri.js movie-this Avatar 
+// node liri.js movie-this Bolero 
+// node liri.js movie-this Despicable Me 
+// node liri.js movie-this Euro Trip 
+// node liri.js movie-this The Last Mimzy 
+// node liri.js movie-this The Life Aquatic with Steve Zissou 
+// node liri.js movie-this The Princess Bride 
+// node liri.js movie-this The Pink Panther 
+// node liri.js movie-this The Wicker Man 
+// node liri.js movie-this Tropic Thunder 
+// node liri.js movie-this Watership Down 
+// node liri.js movie-this The X-Files Fight the Future 
+// node liri.js movie-this The Jerk 
+// node liri.js movie-this The Hunger 
+// node liri.js movie-this Talladega Nights 
+// node liri.js movie-this Straight to Hell 
+// node liri.js movie-this Spaceballs
+
+// node liri.js rando
